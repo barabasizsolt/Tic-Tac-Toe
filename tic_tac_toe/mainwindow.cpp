134ef632249fa729ce0ffa7f3c4086f55c0a638a
barabasizsolt <nevis300@gmail.com>
2021-03-15 01:54:12 +0200

    backend finished.

--- tic_tac_toe/mainwindow.cpp
+++ tic_tac_toe/mainwindow.cpp
@@ -6,10 +6,124 @@ MainWindow::MainWindow(QWidget *parent)
     , ui(new Ui::MainWindow)
 {
     ui->setupUi(this);
+    this->buttonsList = this->findChildren<QPushButton *>();
+    ui->current->setText(this->symbol + " turn!");
+    ui->current->setAlignment(Qt::AlignCenter);
+    setGrid();
 }
 
-MainWindow::~MainWindow()
-{
+MainWindow::~MainWindow(){
     delete ui;
 }
 
+void MainWindow::setGrid(){
+    int ctr = -1;
+    for(int i = 0; i < 3; i++){
+        for(int j = 0; j < 3; j++){
+            grid[i][j] = ctr;
+            --ctr;
+        }
+    }
+}
+
+void MainWindow::changeSimbol(){
+    this->symbol == "X" ? symbol = "0" : symbol = "X";
+    ui->current->setText(this->symbol + " turn!");
+}
+
+void MainWindow::clicked(QPushButton* btn, int x, int y){
+    btn->setText(this->symbol);
+    this->symbol == "X" ? this->grid[x][y] = 0 : this->grid[x][y] = 1;
+    if(gameOver()){
+        QMessageBox::information(this, "Result", "You won the game!");
+        for (int i = 0; i < buttonsList.count() - 1; i++){
+             buttonsList.at(i)->setEnabled(false);
+        }
+        ui->current->setText(this->symbol + " is the winner!");
+    }
+    else {
+        changeSimbol();
+        btn->setEnabled(false);
+        for (int i = 0; i < buttonsList.count() - 1; i++){
+           if(buttonsList.at(i)->isEnabled()){
+               return;
+           }
+        }
+        QMessageBox::information(this, "Result", "Draw!");
+        ui->current->setText("Draw!");
+    }
+}
+
+bool MainWindow::gameOver(){
+    if(grid[0][0] == grid[0][1] && grid[0][1] == grid[0][2]){
+        return true;
+    }
+    if(grid[1][0] == grid[1][1] && grid[1][1] == grid[1][2]){
+        return true;
+    }
+    if(grid[2][0] == grid[2][1] && grid[2][1] == grid[2][2]){
+        return true;
+    }
+    if(grid[0][0] == grid[1][0] && grid[1][0] == grid[2][0]){
+        return true;
+    }
+    if(grid[0][1] == grid[1][1] && grid[1][1] == grid[2][1]){
+        return true;
+    }
+    if(grid[0][2] == grid[1][2] && grid[1][2] == grid[2][2]){
+        return true;
+    }
+    if(grid[0][0] == grid[1][1] && grid[1][1] == grid[2][2]){
+        return true;
+    }
+    if(grid[2][0] == grid[1][1] && grid[1][1] == grid[0][2]){
+        return true;
+    }
+    return false;
+}
+
+void MainWindow::on_buttonReset_clicked(){
+    for (int i = 0; i < buttonsList.count() - 1; i++){
+         buttonsList.at(i)->setEnabled(true);
+         buttonsList.at(i)->setText("");
+    }
+    this->symbol = "X";
+    ui->current->setText(this->symbol + " turn!");
+    setGrid();
+}
+
+void MainWindow::on_button1_clicked(){
+    clicked(ui->button1, 0, 0);
+}
+
+void MainWindow::on_button2_clicked(){
+    clicked(ui->button2, 0, 1);
+}
+
+void MainWindow::on_button3_clicked(){
+    clicked(ui->button3, 0, 2);
+}
+
+void MainWindow::on_button4_clicked(){
+    clicked(ui->button4, 1, 0);
+}
+
+void MainWindow::on_button5_clicked(){
+   clicked(ui->button5, 1, 1);
+}
+
+void MainWindow::on_button6_clicked(){
+   clicked(ui->button6, 1, 2);
+}
+
+void MainWindow::on_button7_clicked(){
+    clicked(ui->button7, 2, 0);
+}
+
+void MainWindow::on_button8_clicked(){
+   clicked(ui->button8, 2, 1);
+}
+
+void MainWindow::on_button9_clicked(){
+   clicked(ui->button9, 2, 2);
+}
--- tic_tac_toe/mainwindow.h
+++ tic_tac_toe/mainwindow.h
@@ -2,6 +2,8 @@
 #define MAINWINDOW_H
 
 #include <QMainWindow>
+#include <QWidget>
+#include <QMessageBox>
 
 QT_BEGIN_NAMESPACE
 namespace Ui { class MainWindow; }
@@ -13,9 +15,45 @@ class MainWindow : public QMainWindow
 
 public:
     MainWindow(QWidget *parent = nullptr);
+
     ~MainWindow();
+
+    void setGrid();
+
+    void clicked(QPushButton* btn, int x, int y);
+
+    void changeSimbol();
+
+    bool gameOver();
+
+private slots:
+    void on_button1_clicked();
+
+    void on_button2_clicked();
+
+    void on_button3_clicked();
+
+    void on_button4_clicked();
+
+    void on_button5_clicked();
+
+    void on_button6_clicked();
+
+    void on_button7_clicked();
+
+    void on_button8_clicked();
+
+    void on_button9_clicked();
+
+    void on_buttonReset_clicked();
 
 private:
     Ui::MainWindow *ui;
+
+    int grid[3][3];
+
+    QString symbol = "X";
+
+    QList<QPushButton *> buttonsList;
 };
 #endif // MAINWINDOW_H
